4bea213307cc8d97cfb268514be23cbcf35180a4
Juliana Silva Barbosa <48034449+julesbarbosa@users.noreply.github.com>
2019-05-07 14:28:00 -0400

    Update clustering.r

--- clustering.r
+++ clustering.r
@@ -35,61 +35,57 @@ data <- scale(data)
 #ggbiplot(pca, groups= kmeans$fac.cluster)
 
 
-install.packages("rgl")
-library(rgl)
-plot3d(pca$x[,1:3], col=kmeans$fac.cluster)
-kmeans$fac.cluster <- as.factor(kmeans$cluster)
 
+#### ELBOW METHOD ####
 
-?ggbiplot
-
-set.seed(123)
 library("purrr")
 # function to compute total within-cluster sum of square 
 wss <- function(k) {
   kmeans(data, k, nstart = 15 )$tot.withinss
 }
 
-# Compute and plot wss for k = 1 to k = 15
-k.values <- 1:25
-
-# extract wss for 2-15 clusters
 wss_values <- map_dbl(k.values, wss)
-
-fviz_nbclust(data, kmeans, method = "wss")
+# Compute and plot wss for k = 1 to k = 15
+k.values <- 1:15
 
 plot(k.values, wss_values,
      type="b", pch = 19, frame = FALSE, 
      xlab="Number of clusters K",
      ylab="Total within-clusters sum of squares")
 
+
+### silhouette method ###
+
+
+# extract wss for 2-15 clusters
+#avg_sil <- function(k) {
+  km.res <- kmeans(df, centers = k, nstart = 25)
+  ss <- silhouette(km.res$cluster, dist(df))
+  mean(ss[, 3])
+}
+
+
+# Compute and plot wss for k = 2 to k = 15
+#k.values <- 2:15
+
+# extract avg silhouette for 2-15 clusters
+#avg_sil_values <- map_dbl(k.values, avg_sil)
+
+
+# plot(k.values, avg_sil_values,
+     type = "b", pch = 19, frame = FALSE, 
+     xlab = "Number of clusters K",
+     ylab = "Average Silhouettes")
+
+##### do not works!
+
 ##### kmenas with 6
 
-kmeans <- kmeans(data, centers = 6, nstart =25)
+kmeans2 <- kmeans(data, centers = 6, nstart =25)
 print(kmeans)
 View(kmeans$cluster)
 
-## sample for plot
-data.sample.index <- sample(1:nrow(data),1000)
-data.sample <- data[data.sample.index,]
-
-kmenas.sam
-kmeans.sample <- kmeans$cluster[data.sample.index]
 
 fviz_cluster(kmeans, data = data, geom=c("point"), ellipse.alpha  = 0.05)
-write.csv(datast, "clusterdf.csv")
-qplot(datast$physhlth, datast$menthlth, colour = datast$`kmeans$cluster`, 
-      data = datast)
-plot(datast$`kmeans$cluster`, datast$`kmeans$cluster`)
-
-
-datacluster$X.1 <- NULL
-datacluster$kmeans.cluster <- NULL
-datacluster$HealthSurvey.sex.1 <- NULL
-datacluster$X <- NULL
-datacluster$clusternum <- NULL
-new_cluster <- cbind(datacluster, kmeans$cluster)
-
-
 
 write.csv(new_cluster, "6cluster_data.csv")
